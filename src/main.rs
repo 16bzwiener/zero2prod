8ccb2f9d9528ad815719bd6f668fbf6b5e4211d8
Ben Zwiener <benjamin.zwiener@gmail.com>
2024-02-17 16:52:51 -0600

    basic logs and tracing implemented: secton 4.5.2

--- src/main.rs
+++ src/main.rs
@@ -2,10 +2,15 @@ use zero2prod::startup::run;
 use zero2prod::configuration::get_configuration;
 use std::net::TcpListener;
 use sqlx::PgPool;
+use env_logger::Env;
 
 
 #[tokio::main]
 async fn main() -> Result<(), std::io::Error> {
+    // We are falling back to printing all logs at info-level or above
+    // if the RUST_LOG environment variable  has not been set
+    env_logger::Builder::from_env(Env::default().default_filter_or("info")).init();
+
     let configuration = get_configuration().expect("Failed to read configuration.");
 
     let connection_pool = PgPool::connect(
