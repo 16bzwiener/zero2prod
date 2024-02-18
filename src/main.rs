9031f96e5bd950d9c5c7f8b9360372a4cf4810f5
Ben Zwiener <benjamin.zwiener@gmail.com>
2024-02-17 19:13:13 -0600

    section 4.5.8

--- src/main.rs
+++ src/main.rs
@@ -6,10 +6,13 @@ use env_logger::Env;
 use tracing::subscriber::set_global_default;
 use tracing_bunyan_formatter::{BunyanFormattingLayer, JsonStorageLayer};
 use tracing_subscriber::{layer::SubscriberExt, EnvFilter, Registry};
+use tracing_log::LogTracer;
 
 
 #[tokio::main]
 async fn main() -> Result<(), std::io::Error> {
+    // redirect all logs events to our subscriber
+    LogTracer::init().expect("Failed to set logger");
 
     let env_filter = EnvFilter::try_from_default_env()
         .unwrap_or_else(|_| EnvFilter::new("info"));
@@ -23,7 +26,6 @@ async fn main() -> Result<(), std::io::Error> {
         .with(env_filter)
         .with(JsonStorageLayer)
         .with(formatting_layer);
-
     set_global_default(subscriber).expect("Failed to set subscriber");
 
     let configuration = get_configuration().expect("Failed to read configuration.");
